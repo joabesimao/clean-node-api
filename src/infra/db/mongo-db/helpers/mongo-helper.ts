80f6a1e154d6244cc3865894ed70dab3365898e2
joabesimao <simaojoabecosta@gmail.com>
2024-11-10 16:02:28 -0300

    feat: connect to mongodb before integration tests

--- src/infra/db/mongo-db/helpers/mongo-helper.ts
+++ src/infra/db/mongo-db/helpers/mongo-helper.ts
@@ -3,10 +3,8 @@ import { Collection, MongoClient } from "mongodb";
 export const MongoHelper = {
   client: null as MongoClient,
 
-  async connect(url: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL, {
-      maxPoolSize: 100,
-    });
+  async connect(url: any): Promise<void> {
+    this.client = await MongoClient.connect(url, {});
   },
 
   async disconnect(): Promise<void> {
