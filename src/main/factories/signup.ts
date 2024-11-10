4b92f00e0a6fd5094be0a72f02c399c9a447a73b
joabesimao <simaojoabecosta@gmail.com>
2024-11-10 16:30:54 -0300

    feat: add ExpressRouteAdapter

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from "../../presentation/controllers/signup/signUpController";
+import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
+import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
+import { AccountMongoRepository } from "../../infra/db/mongo-db/account-repository/account-mongo-repository";
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12;
+  const emailValidatorAdapter = new EmailValidatorAdapter();
+  const bcryptAdapter = new BcryptAdapter(salt);
+  const accountMongoRepository = new AccountMongoRepository();
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
+  const signUpController = new SignUpController(
+    emailValidatorAdapter,
+    dbAddAccount
+  );
+  return signUpController;
+};
