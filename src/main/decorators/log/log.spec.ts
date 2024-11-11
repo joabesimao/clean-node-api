7011b359c6a4448d7db53cae4632d4efdb1d7005
joabesimao <simaojoabecosta@gmail.com>
2024-11-11 20:17:57 -0300

    feat: ensure LogController decorator returns the same result of the controller

--- src/main/decorators/log/log.spec.ts
+++ src/main/decorators/log/log.spec.ts
@@ -49,4 +49,24 @@ describe("Log Controller Decorator ", () => {
     await sut.handle(httpRequest);
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
   });
+
+  test("Should return the same result of the controller ", async () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        email: "any_email@email.com",
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const result = await sut.handle(httpRequest);
+    expect(result).toEqual({
+      body: {
+        name: "joabe",
+      },
+      statusCode: 200,
+    });
+  });
 });
