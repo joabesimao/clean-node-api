4bb927ace6921470425400753f937a1605fade09
joabesimao <simaojoabecosta@gmail.com>
2024-11-13 19:08:41 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongo-db/log-repository/log.spec.ts
@@ -0,0 +1,26 @@
+import { Collection } from "mongodb";
+import { LogMongoRepository } from "./log";
+import { MongoHelper } from "../helpers/mongo-helper";
+
+describe("Log Mongo Repository", () => {
+  let logErrorsCollection: Collection;
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    logErrorsCollection = await MongoHelper.getCollection("errors");
+    await logErrorsCollection.deleteMany({});
+  });
+
+  test("Should create an error log on success", async () => {
+    const sut = new LogMongoRepository();
+    await sut.log("any_error");
+    const count = await logErrorsCollection.countDocuments();
+    expect(count).toBe(1);
+  });
+});
