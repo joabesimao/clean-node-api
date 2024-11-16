49260703de2cf31707a1d250b87dc0bb34c49300
joabesimao <simaojoabecosta@gmail.com>
2024-11-16 19:17:07 -0300

    feat: ensure LoginController return 200 if valid credentials are provided

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -7,6 +7,7 @@ import {
 } from "../login/login-protocols";
 import {
   badRequest,
+  ok,
   serverError,
   unauthorized,
 } from "../../helpers/http-helper";
@@ -41,6 +42,7 @@ export class LoginController implements Controller {
       if (!accessToken) {
         return unauthorized();
       }
+      return ok({ accessToken });
     } catch (error) {
       return serverError(error);
     }
